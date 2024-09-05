811f381b77262c72bad89760f6528ca9ad9ca646
herberth <herberthcaldeira@gmail.com>
2024-09-05 20:33:23 -0300

    contact list

--- app/Http/Controllers/Contact/ContactController.php
+++ app/Http/Controllers/Contact/ContactController.php
@@ -9,11 +9,17 @@ use App\Http\Resources\ContactResource;
 use App\Models\Agenda;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Response;
 
 class ContactController extends Controller
 {
+    public function index(Request $request, Agenda $agenda): AnonymousResourceCollection
+    {
+        return ContactResource::collection(Contact::query()->with(['agenda'])->where(['agenda_id' => $agenda->id])->paginate());
+    }
+
     public function store(StoreContactRequest $request, Agenda $agenda): JsonResource
     {
         $item = Contact::create($request->validated());
---
+++ tests/Feature/Contact/IndexContactTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use App\Models\Agenda;
+use App\Models\Contact;
+use App\Models\User;
+
+it('should be able to return list of contacts', function () {
+
+    $user = User::factory()->create();
+
+    $agenda = Agenda::factory()->create();
+
+    $contact = Contact::factory(15)->for($agenda)->create();
+
+    $this->actingAs($user);
+
+    $response = $this->getJson(route('contact.index', ['agenda' => $agenda]));
+
+    ds($response->json());
+
+    $response->assertOk();
+
+    $response->assertJsonCount(15, 'data');
+
+});
